2381e126c20364cd84f4dfffe6a2f74d2356aff6
sumit27x <sumitgupta27x@gmail.com>
2024-10-06 22:24:54 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), sentryVitePlugin({
-    org: "sumit-gupta",
-    project: "iphone-animation",
-    base:"/iphone-animation/"
-  })],
-
+  plugins: [
+    react(),
+    sentryVitePlugin({
+      org: "sumit-gupta",
+      project: "iphone-animation",
+      // Ensure to set the right configuration for your production environment
+      release: process.env.SENTRY_RELEASE || '1.0.0', // Optionally set your release version
+      sourcemaps: {
+        // This is necessary for Sentry to find your source maps
+        upload: {
+          include: ['./dist'], // Include your build directory
+          // Adjust this if your build folder is different
+        },
+      },
+    }),
+  ],
+  base: '/iphone-animation/', // Correct base URL for GitHub Pages
   build: {
-    sourcemap: true
-  }
-})
+    sourcemap: true, // Enable sourcemaps for debugging
+  },
+});
