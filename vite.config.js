bcd9a2ea8f16d61751a27dc91ffce7473ae7e55a
sumit27x <sumitgupta27x@gmail.com>
2024-10-06 22:15:19 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react(), sentryVitePlugin({
     org: "sumit-gupta",
-    project: "iphone-animation"
+    project: "iphone-animation",
+    base:"/iphone-animation/"
   })],
 
   build: {
